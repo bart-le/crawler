a3b84c086c49455f97802ea3e1d10319772303dd
Bart Le <bart@pj.edu.pl>
2022-03-10 18:15:12 +0100

    Implement missing methods

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Crawler
@@ -24,6 +27,47 @@ namespace Crawler
 			catch (IndexOutOfRangeException exception)
 			{
 				throw new ArgumentNullException("Podaj 1 argument", exception);
+			}
+		}
+
+		private static List<string> GetEmails(string html, string pattern)
+		{
+			return Regex.Matches(html, pattern)
+				.Cast<Match>()
+				.Select(match => match.Value)
+				.Distinct()
+				.ToList();
+		}
+
+		private static void DisplayEmails(List<string> emails)
+		{
+			if (emails.Count > 0)
+			{
+				foreach (string email in emails)
+					Console.WriteLine(email);
+			}
+			else
+			{
+				Console.WriteLine("Nie znaleziono adresów email");
+			}
+		}
+
+		private static async Task<string> GetURLContent(string url)
+		{
+			if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+				throw new ArgumentException("Błedny URL");
+
+			using HttpClient httpClient = new();
+
+			try
+			{
+				HttpResponseMessage response = await httpClient.GetAsync(url);
+
+				return await response.Content.ReadAsStringAsync();
+			}
+			catch (HttpRequestException)
+			{
+				throw new HttpRequestException("Błąd w czasie pobierania strony");
 			}
 		}
 	}
