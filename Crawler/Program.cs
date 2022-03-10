4828a73fcd9c4c489df3fd4cf3ac44f2fd692fe6
Bart Le <bart@pj.edu.pl>
2022-03-10 17:00:00 +0100

    Add core function calls

---
+++ Crawler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			try
+			{
+				string url = args[0];
+
+				string content = await GetURLContent(url);
+
+				if (content != null)
+				{
+					List<string> emails = GetEmails(content, @"[\w\.-]+@[\w\.-]+\.\w+");
+
+					DisplayEmails(emails);
+				}
+			}
+			catch (IndexOutOfRangeException exception)
+			{
+				throw new ArgumentNullException("Podaj 1 argument", exception);
+			}
+		}
+	}
+}
